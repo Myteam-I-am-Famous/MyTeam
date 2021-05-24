222e57b66ddd78b5c91f252d6dc0c6389cd3a47a
Abdou <abdou.dia31@outlook.fr>
2021-05-24 15:24:31 +0200

    code_check fixes

--- includes/code_check.php
+++ includes/code_check.php
@@ -1,9 +1,22 @@
 <?php
-if(!isset($_GET['code']))
-{
+
+include "./database_handler.php";
+include 'functions.php';
+
+if (!isset($_GET['code'])) {
 	header('location: ../index.php?message=nocode');
 	exit();
-
 }
 
 echo '<h1>Votre code est ' . $_GET['code'] . '</h1>';
+
+echo 'useractivated : <br>';
+echo useractivated($dbh, 'abdoudu78130@gmail.com');
+
+echo '<br><br>userhash : <br>';
+echo getuserinfo($dbh, 'hash', 'abdoudu78130@gmail.com');
+
+echo '<br><br>hashedcode : <br>';
+echo md5($_GET['code']);
+
+echo '<br>';
--- includes/functions.php
+++ includes/functions.php
@@ -54,49 +54,99 @@ function userexists($dbh, $email)
 function getmaxID($dbh)
 {
 
-	$q = 'SELECT MAX(id) FROM utilisateurs';
-	$req = $dbh->prepare($q);
-	$status = $req->execute();
+    $q = 'SELECT MAX(id) FROM utilisateurs';
+    $req = $dbh->prepare($q);
+    $status = $req->execute();
 
-	if($status){
-		$result = $req->fetch();
-		return $result[0];
-	}else{
-		return false;
-	}
-
-
+    if ($status) {
+        $result = $req->fetch();
+        return $result[0];
+    } else {
+        return false;
+    }
 }
 
 function sendVerificationMail($to, $code)
 {
     $mail = new PHPMailer(true);
 
-try{
-    $mail->isSMTP();
-    $mail->SMTPAuth = false;
-    $mail->SMTPSecure = false;
-    $mail->SMTPAutoTLS = false;
+    try {
+        $mail->isSMTP();
+        $mail->SMTPAuth = false;
+        $mail->SMTPSecure = false;
+        $mail->SMTPAutoTLS = false;
 
-    $mail->Host = 'localhost';
-    $mail->Port = 25;
+        $mail->Host = 'localhost';
+        $mail->Port = 25;
 
-    $mail->isHTML(true);
-    $mail->SetFrom('no-reply@myteam.fr','[MyTeam]');
-    $mail->Subject = "Confirmation d'inscription";
-    $mail->Body = 'Votre code de vérification est :' 
-. $code 
-. "<br><a href='141.94.17.218/includes/code_check.php?type=signup&code={$code}'>Cliquez sur ce lien pour vérifier votre compte</a>";
-    $mail->CharSet = 'utf-8';
+        $mail->isHTML(true);
+        $mail->SetFrom('no-reply@myteam.fr', '[MyTeam]');
+        $mail->Subject = "Confirmation d'inscription";
+        $mail->Body = 'Votre code de vérification est :'
+            . $code
+            . "<br><a href='141.94.17.218/includes/code_check.php?type=signup&code={$code}'>Cliquez sur ce lien pour vérifier votre compte</a>";
+        $mail->CharSet = 'utf-8';
 
-    $mail->AddAddress($to);
+        $mail->AddAddress($to);
 
-    $mail->Send();
-    echo 'Mail sent';
-}catch(Exception $e) {
-    echo 'Message could not be sent. Mail Error: ' . $mail->ErrorInfo;
+        $mail->Send();
+        echo 'Mail sent';
+    } catch (Exception $e) {
+        echo 'Message could not be sent. Mail Error: ' . $mail->ErrorInfo;
+    }
 }
+
+function checkVerificationCode($dbh, $email, $code)
+{
+    if (!userexists($dbh, $email))
+        return false;
+
+    if (useractivated($dbh, $email))
+        return false;
 }
+
+function useractivated($dbh, $email)
+{
+
+
+    $q = 'SELECT actif FROM utilisateurs WHERE email = :email;';
+    $stmt = $dbh->prepare($q);
+
+    $status = $stmt->execute(
+        array(
+            'email' => $email
+        )
+    );
+
+    if ($status) {
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $result['actif'];
+    }
+
+    return false;
+}
+
+function getuserinfo($dbh, $info, $email)
+{
+    $q = 'SELECT ' . $info . ' FROM utilisateurs WHERE email = :email;';
+    $stmt = $dbh->prepare($q);
+
+    $status = $stmt->execute(
+        array(
+            'email' => $email
+        )
+    );
+
+    if ($status) {
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $result[$info];
+    }
+
+    return false;
+}
+
 
 // * End Utils
 
@@ -114,30 +164,32 @@ function invalidAge($age)
 
 function createUser($dbh, $nom, $prenom, $age, $email, $mdp)
 {
-    $q = 'INSERT INTO utilisateurs (id, nom, prenom, age, email, mdp, hash) VALUES(:id, :nom, :prenom, :age, :email, :mdp, :hash);';
+    $q = 'INSERT INTO utilisateurs (id, nom, prenom, age, email, mdp, hash, actif) VALUES(:id, :nom, :prenom, :age, :email, :mdp, :hash, :actif);';
     $stmt = $dbh->prepare($q);
 
-    $hash = md5(rand(0, 1000));
+    $code = rand(0, 1000);
+    $hash = md5($code);
 
 
 
-   $id = getmaxID($dbh) + 1;
+    $id = getmaxID($dbh) + 1;
 
 
     $status = $stmt->execute(
         array(
-	    'id' => $id,
+            'id' => $id,
             'nom' => $nom,
             'prenom' => $prenom,
             'age' => $age,
             'email' => $email,
             'mdp' => password_hash($mdp, PASSWORD_DEFAULT),
-            'hash' => $hash
+            'hash' => $hash,
+            'actif' => 0
         )
     );
 
     if ($status) {
-        sendVerificationMail($email, $hash);
+        sendVerificationMail($email, $code);
         return true;
     }
 
@@ -172,4 +224,3 @@ function validpassword($dbh, $email, $mdp)
 }
 
 // * End Login
-
